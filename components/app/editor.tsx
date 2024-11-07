23a9245e77aebd06c141641337a7157c8bbdfa67
Suganya <satish860@gmail.com>
2024-11-07 17:12:58 +0530

    List Plugin

--- components/app/editor.tsx
+++ components/app/editor.tsx
@@ -12,7 +12,11 @@ import {
   SubscriptPlugin,
   SuperscriptPlugin,
 } from "@udecode/plate-basic-marks/react";
-import { HeadingPlugin } from "@udecode/plate-heading/react";
+import { IndentPlugin } from '@udecode/plate-indent/react';
+import { IndentListPlugin } from '@udecode/plate-indent-list/react';
+import { HEADING_KEYS } from '@udecode/plate-heading';
+import { HeadingPlugin } from '@udecode/plate-heading/react';
+import { ParagraphPlugin } from '@udecode/plate-common/react';
 import { BlockquotePlugin } from "@udecode/plate-block-quote/react";
 import { CodeBlockPlugin } from "@udecode/plate-code-block/react";
 import { createPlateUI } from "@/lib/create-plate-ui";
@@ -26,8 +30,10 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { FloatingToolbar } from "@/components/plate-ui/floating-toolbar";
 import { FloatingToolbarButtons } from "@/components/plate-ui/floating-toolbar-buttons";
 import { CursorOverlay } from "@/components/plate-ui/cursor-overlay";
+import { AutoformatPlugin } from '@udecode/plate-autoformat/react';
 
 import { SlashPlugin } from '@udecode/plate-slash-command/react';
+import { ListPlugin } from '@udecode/plate-list/react';
 
 
 export default function BasicEditor() {
@@ -58,11 +64,27 @@ export default function BasicEditor() {
       SubscriptPlugin,
       SuperscriptPlugin,
       HeadingPlugin,
+      IndentPlugin.configure({
+        inject: {
+          targetPlugins: [ParagraphPlugin.key, HEADING_KEYS.h1],
+        }
+      }),
+      IndentListPlugin.configure({
+        inject: {
+          targetPlugins: [ParagraphPlugin.key, HEADING_KEYS.h1],
+        }
+      }),
       BlockquotePlugin,
       CodeBlockPlugin,
       SlashPlugin,
+      ListPlugin,
       DndPlugin.configure({
         options: { enableScroller: true },
+      }),
+      AutoformatPlugin.configure({
+        options: {
+          enableUndoOnDelete: true,
+        },
       }),
     ],
     override: {
